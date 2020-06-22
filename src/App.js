646e6e00c3d6d58dca89af33a030d1f5169e1fa3
Stephen Michel <git@smichel.me>
2020-06-22 11:13:04 -0400

    Minimal *working* example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { ComboChart } from "@toast-ui/react-chart";
+import 'tui-chart/dist/tui-chart.css'
 
 function App() {
+  let data = {
+    categories: [ 1, 2, 3, 4, 5 ],
+    series: {
+      area: [
+        {
+          name: "Goal",
+          data: [ 3, 3, 3, 3, 3 ],
+        },
+      ],
+      line: [
+        {
+          name: "Data",
+          data: [ 5, 4, 3, 2, 1 ],
+        },
+      ],
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ComboChart data={data} />
     </div>
   );
 }
