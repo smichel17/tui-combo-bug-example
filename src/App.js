4850935d56b56702905d5a09c277350b71b35618
Stephen Michel <git@smichel.me>
2020-06-22 11:38:30 -0400

    Minimal *non-working* Area Chart (coordinate data)

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React from 'react';
 
-import { ComboChart } from "@toast-ui/react-chart";
+import { AreaChart } from "@toast-ui/react-chart";
 import 'tui-chart/dist/tui-chart.css'
 
 function App() {
   let data = {
-    series: {
-      area: [
-        {
-          name: "Goal",
-          data: [
-            { x: 1, y: 3, },
-            { x: 2, y: 3, },
-            { x: 3, y: 3, },
-            { x: 4, y: 3, },
-            { x: 5, y: 3, },
-          ],
-        },
-      ],
-      line: [
-        {
-          name: "Data",
-          data: [
-            { x: 1, y: 5, },
-            { x: 2, y: 4, },
-            { x: 3, y: 3, },
-            { x: 3, y: 2, },
-            { x: 3, y: 1, },
-          ],
-        },
-      ],
-    },
+    series: [
+      {
+        name: "Goal",
+        data: [
+          { x: 1, y: 3, },
+          { x: 2, y: 3, },
+          { x: 3, y: 3, },
+          { x: 4, y: 3, },
+          { x: 5, y: 3, },
+        ],
+      },
+    ],
   };
 
-  return <ComboChart data={data} />;
+  return <AreaChart data={data} />;
 }
 
 export default App;
